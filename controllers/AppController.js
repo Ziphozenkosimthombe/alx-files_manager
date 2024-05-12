f4f009fb4d2efc746a71d3cf2af955c2f336c331
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-12 06:18:22 -0700

    fixing the bug on routes folder and controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,13 +4,15 @@ import dbClient from "../utils/db.js";
 class AppControllers {
   static getStatus(_req, res) {
     if (dbClient.isAlive() && redisClient.isAlive()) {
-      return res.status(200).json({ redis: true, db: true });
+      res.status(200).json({ redis: true, db: true });
+      res.end();
     }
   }
   static async getStats(_req, res) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
-    return res.status(200).json({ users, files });
+    res.status(200).json({ users, files });
+    res.end();
   }
 }
 
