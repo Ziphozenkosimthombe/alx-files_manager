79b047f001a4af848f787dec8491a56510fcce6b
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-12 12:04:19 +0200

    db.js

--- utils/db.js
+++ utils/db.js
@@ -1,36 +1,46 @@
-import mongodb from 'mongodb';
-import {MongoClient} from 'mongodb';
+#!/usr/bin/env node
 
-class DBClient{
-    constructor(){
-        this.host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
-        this.port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
-        this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
-        const url = `mongodb://${this.host}:${this.port}/${this.database}`;
-        this.connected = false;
-        this.client = new MongoClient(url, {useUnifiedTopology: true});
-        this.client.connect().then(() => {
-            this.connected = true;
-        }).catch((err) => console.log(err));
-    }
+import pkg from "mongodb";
+const { MongoClient } = pkg;
 
-    isAlive(){
-        return this.connected;
-    }
-    
-    // return the number of documents in users collection
-    async nbUsers(){
-        await this.client.connect();
-        const collectUsers = this.client.db(this.database).collection('users').countDocuments();
-        return collectUsers;
-    }
-    
-    // return the number of documents in the files collection
-    async nbFiles(){
-        await this.client.connect();
-        const collectFiles = this.client.db(this.database).collection('files').countDocuments();
-        return collectFiles;
-    }
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || "localhost";
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || "files_manager";
+    this.connected = false;
+    this.url = `mongodb://${this.host}:${this.port}/${this.database}`;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.client
+      .connect()
+      .then(() => {
+        this.connected = true;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client
+      .db(this.database)
+      .collection("users")
+      .countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    await this.client.connect();
+    const files = await this.client
+      .db(this.database)
+      .collection("files")
+      .countDocuments();
+    return files;
+  }
 }
 
 const dbClient = new DBClient();
