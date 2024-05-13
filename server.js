7f87b25e77d4c5898bbed376412402e537f95c74
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-13 06:24:51 -0700

    server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import mainRoutes from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = (process.env.PORT) ? process.env.PORT : 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(mainRoutes);
 
 app.use('/', mainRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
