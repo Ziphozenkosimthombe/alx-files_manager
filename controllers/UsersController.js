3d26f22de81ab3f1dfb4be801f09411ffc15c3dc
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-13 09:45:31 +0200

    UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,29 +1,41 @@
 import dbClient from '../utils/db.js';
 
 class UsersController {
-  static async postNew (req, res) {
+  static async postNew(req, res) {
     try {
       const { email, password } = req.body;
+
       if (!email) {
         res.status(400).json({ error: 'Missing email' });
+        res.end();
+        return;
       }
+
       if (!password) {
         res.status(400).json({ error: 'Missing password' });
+        res.end();
+        return;
       }
 
       const exists = await dbClient.userExists(email);
+
       if (exists) {
         res.status(400).json({ error: 'User already exists' });
+        res.end();
+        return;
       }
 
       const newUser = await dbClient.createUser(email, password);
+
+      await newUser.save();
+
       if (newUser) {
         res.status(201).json({
           _id: newUser._id,
-          email: newUser.email
+          email: newUser.email,
         });
       } else {
-        res.status(500).json({ error: 'Error creating user' });
+        res.status(400).json({ error: 'Invalid user data' });
       }
     } catch (err) {
       res.status(500).json({ error: err.message });
