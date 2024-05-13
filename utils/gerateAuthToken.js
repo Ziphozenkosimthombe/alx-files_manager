7ed44886e676e8f4ab07a4dfb3a1382359afe358
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-13 19:15:19 +0200

    utils/

--- utils/gerateAuthToken.js
+++ utils/gerateAuthToken.js
@@ -1,31 +1,35 @@
-export const getAuthzHeader = (req) => {
-  const header = req.headers.authorization;
-  if (!header) {
-    return null;
+class GenerateAuthToken {
+  static getAuthzHeader(req) {
+    const header = req.headers.authorization;
+    if (!header) {
+      return null;
+    }
+    return header;
   }
-  return header;
-};
 
-export const getToken = (authzHeader) => {
-  const tokenType = authzHeader.substring(0, 6);
-  if (tokenType !== 'Basic ') {
-    return null;
+  static getToken(authzHeader) {
+    const tokenType = authzHeader.substring(0, 6);
+    if (tokenType !== 'Basic ') {
+      return null;
+    }
+    return authzHeader.substring(6);
   }
-  return authzHeader.substring(6);
-};
 
-export const decodeToken = (token) => {
-  const decodedToken = Buffer.from(token, 'base64').toString('utf8');
-  if (!decodedToken.includes(':')) {
-    return null;
+  static decodeToken(token) {
+    const decodedToken = Buffer.from(token, 'base64').toString('utf8');
+    if (!decodedToken.includes(':')) {
+      return null;
+    }
+    return decodedToken;
   }
-  return decodedToken;
-};
 
-export const getCredentials = (decodedToken) => {
-  const [email, password] = decodedToken.split(':');
-  if (!email || !password) {
-    return null;
+  static getCredentials(decodedToken) {
+    const [email, password] = decodedToken.split(':');
+    if (!email || !password) {
+      return null;
+    }
+    return { email, password };
   }
-  return { email, password };
-};
+}
+
+export default GenerateAuthToken;
