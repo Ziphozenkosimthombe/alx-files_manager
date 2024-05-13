e50f271443123a4afe33433c378162652617c49e
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-13 06:36:42 -0700

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 import express from 'express';
-
-import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js';
-import AuthController from '../controllers/AuthController.js';
-
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
@@ -11,9 +6,6 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
 
 export default router;
