9b9d0cc99b26817d8c0cdd0e373a60518ba0c3ee
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-13 06:00:41 -0700

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mainRoutes from './routes/index.js';
+import mainRoutes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
