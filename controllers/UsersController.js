e839a343ec6f7ba2dbf230ac5747b86e054975b1
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-13 11:23:24 +0200

    UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,40 +1,33 @@
 import dbClient from '../utils/db.js';
 
 class UsersController {
-  static async postNew (req, res) {
-    const { email, password } = req.body;
+  static async postNew(req, res) {
+    try {
+      const { email, password } = req.body;
 
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      res.end();
-      return;
-    }
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
 
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      res.end();
-      return;
-    }
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
 
-    const exists = await dbClient.userExists(email);
+      const exists = await dbClient.userExists(email);
 
-    if (exists) {
-      res.status(400).json({ error: 'User already exists' });
-      res.end();
-      return;
-    }
+      if (exists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
 
-    const newUser = await dbClient.createUser(email, password);
+      const newUser = await dbClient.createUser(email, password);
+      await newUser.save();
 
-    await newUser.save();
-
-    if (newUser) {
-      res.status(201).json({
+      return res.status(201).json({
         _id: newUser._id,
-        email: newUser.email
+        email: newUser.email,
       });
-    } else {
-      res.status(400).json({ error: 'Invalid user data' });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
   }
 }
