42b797a27125f2d73d734a31e0357f023dda0848
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-13 20:17:40 +0200

    server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import mainRoutes from './routes/index';
 
 const app = express();
-const PORT = (process.env.PORT) ? process.env.PORT : 5000;
+const PORT = process.env.PORT ? process.env.PORT : 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(mainRoutes);
 
 app.use('/', mainRoutes);
