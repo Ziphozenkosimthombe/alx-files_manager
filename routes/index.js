e5462d915ee795ea9f5232766ed093b9f4d104bc
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-14 20:15:13 +0200

    index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -13,5 +13,5 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
-
+router.post('/files', FilesController.postUpload);
 export default router;
