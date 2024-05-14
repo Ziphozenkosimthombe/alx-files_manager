faccb21ba1abb93c72d039dcffd1a33c63eff90d
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-14 19:12:55 +0200

    AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,39 +4,59 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AuthController {
-  static async getConnect(request, response) {
-    const authData = request.header('Authorization');
-    let userEmail = authData.split(' ')[1];
-    const buff = Buffer.from(userEmail, 'base64');
-    userEmail = buff.toString('ascii');
-    const data = userEmail.split(':'); // contains email and password
-    if (data.length !== 2) {
-      response.status(401).json({ error: 'Unauthorized' });
-      return;
-    }
-    const hashedPassword = sha1(data[1]);
-    const users = dbClient.db.collection('users');
-    users.findOne({ email: data[0], password: hashedPassword }, async (err, user) => {
-      if (user) {
-        const token = uuidv4();
-        const key = `auth_${token}`;
-        await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
-        response.status(200).json({ token });
-      } else {
-        response.status(401).json({ error: 'Unauthorized' });
+  static async getConnect(req, res) {
+    try {
+      const authData = req.header('Authorization');
+      if (!authData) {
+        res.status(401).json({ error: 'Authorization header missing' });
       }
-    });
+
+      let userEmail = authData.split(' ')[1];
+      const buff = Buffer.from(userEmail, 'base64');
+      userEmail = buff.toString('ascii');
+      const data = userEmail.split(':'); // contains email and password
+      if (data.length !== 2) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+      }
+
+      const hashedPassword = sha1(data[1]);
+      const users = dbClient.db.collection('users');
+      const user = await users.findOne({
+        email: data[0],
+        password: hashedPassword,
+      });
+
+      if (!user) {
+        res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const token = uuidv4();
+      const key = `auth_${token}`;
+      await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(401).json({ error: error.message });
+    }
   }
 
-  static async getDisconnect(request, response) {
-    const token = request.header('X-Token');
-    const key = `auth_${token}`;
-    const id = await redisClient.get(key);
-    if (id) {
+  static async getDisconnect(req, res) {
+    try {
+      const token = req.header('X-Token');
+      if (!token) {
+        res.status(401).json({ error: 'X-Token header missing' });
+      }
+
+      const key = `auth_${token}`;
+      const id = await redisClient.get(key);
+      if (!id) {
+        res.status(401).json({ error: 'Unauthorized' });
+      }
+
       await redisClient.del(key);
-      response.status(204).json({});
-    } else {
-      response.status(401).json({ error: 'Unauthorized' });
+      res.status(204).json({});
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
   }
 }
